c9fd122e505d7a9b736301ea360e7dc2a7224a8a
henrique-al <henrique.a.lazzarino@gmail.com>
2023-08-18 16:24:05 -0300

    funcionando

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,22 +7,24 @@ const Game = () => {
   const { answer, board, setBoard } = useContext(GameContext);
 
   useEffect(() => {
-    if (answer) {
+    if (board.length===0&&answer) {
       const createInitialBoard = () => {
         const rows = 6;
         const columns = answer.length;
         const initialBoard = Array.from({ length: rows }, () =>
-          Array(columns).fill(null)
+        Array(columns).fill(null)
         );
         return initialBoard;
       };
-
+      
       setBoard(createInitialBoard());
       console.log(answer)
       console.log(answer)
-      console.log(board)
+      
     }
   }, [answer]);
+
+  console.log(board)
 
   return <div></div>;
 };
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -6,6 +6,34 @@ const GameContextProvider = ({ children }) => {
   const [answer, setAnswer] = useState(null);
   const [board, setBoard] = useState([])
 
+  useEffect(() => {
+      const getRandomChampion = (array) => {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+      };
+      
+      const removeSpecialCharsAndSpaces = (str) => {
+        return str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+      };
+      
+      const fetchChampions = async () => {
+          try {
+            const response = await fetch("http://localhost:3000/champions");
+            if (!response.ok) {
+              throw new Error("Failed to fetch champions data");
+            }
+            const championsData = await response.json();
+            const championsArray = championsData.map(champion => champion.name);
+            const randomChampion = getRandomChampion(championsArray);
+            setAnswer(removeSpecialCharsAndSpaces(randomChampion).toUpperCase());
+          } catch (error) {
+            console.error(error);
+          }
+      };
+      
+      fetchChampions();
+  }, []);
+
   return (
     <GameContext.Provider value={{ answer, setAnswer, board, setBoard }}>
       {children}
