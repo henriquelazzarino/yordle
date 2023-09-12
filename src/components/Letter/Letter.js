1597c502a37f87371db8cd0300eae66281637805
henrique-al <henrique.a.lazzarino@gmail.com>
2023-09-11 21:53:42 -0300

    Update Letter.js

--- src/components/Letter/Letter.js
+++ src/components/Letter/Letter.js
@@ -16,15 +16,13 @@ const Letter = ({ y, attempt }) => {
   useEffect(() => {
     setClassName(() => {
       let className = "Letter ";
-      if (at1 === attempt) {
-        if (correctLetters.includes(board[attempt][y])) {
+        if (disabledLetters.includes(board[attempt][y])) {
           className += " disabled";
         } else if (wrongPosLetters.includes(board[attempt][y])) {
           className += " wrong-position";
-        } else if (disabledLetters.includes(board[attempt][y])) {
+        } else if (correctLetters.includes(board[attempt][y])) {
           className += " correct";
         }
-      }
       return className;
     });
   }, [board, attempt]);
